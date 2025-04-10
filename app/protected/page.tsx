775bb300842abc98b30c984de80ca72dec49a8dc
Kanmi Obasa <kanmi@konfirmed.com>
2025-04-10 05:12:24 -0700

    Remove unused createClient imports and related code for authentication actions

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,30 +1,21 @@
-import { createClient } from "@/utils/supabase/server";
+// import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return redirect("/sign-in");
-  }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
-          Welcome back, {user.user_metadata.full_name || user.email}!
+          {/* Welcome back, {user.user_metadata.full_name || user.email}! */}
         </div>
       </div>
       <div className="bg-accent p-6 rounded-md">
         <h1 className="text-3xl font-bold mb-2">Names of God</h1>
       {/* Add names of GOD from supabase */}
-      
+
       </div>
     </div>
   );
